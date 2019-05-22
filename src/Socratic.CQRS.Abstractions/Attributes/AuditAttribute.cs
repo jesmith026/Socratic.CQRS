5890e6b8dedd60e64e4a1c6d19df7d60c479a198
James Smith <jesmith026@outlook.com>
2019-05-21 23:20:43 -0500

    move implementation out of abstraction package

--- src/Socratic.CQRS.Abstractions/Attributes/AuditAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Socratic.CQRS.Abstractions.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public sealed class AuditAttribute : CqrsAttribute
-    {
-    }
-}
